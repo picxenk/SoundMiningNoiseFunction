1ff643f44e67c61c8749820628b797fabde925ef
Seungbum Kim <picxenk@gmail.com>
2021-10-30 21:00:55 +0900

    SMNF01

--- SMNF_p5/sketch.js
+++ SMNF_p5/sketch.js
@@ -1,58 +1,47 @@
-let osc;
+let osc, freq;
 let t, tic;
-let freq, d;
 let keys = [];
-
 let isPlaying = false;
 
 function setup() {
   createCanvas(50, 50);
-  background(200);
-
+  background(220);
   osc = new p5.Oscillator('sine');
-  freq = 300;
+  freq = 777;
 }
 
 function draw() {
   background(freq/tic);
+  text(char(t), width/2, height/2);
   if (isPlaying) {
-    // dig(10, 16, 2000, 1);
-    // dig(107, 8, 20000, 0.01);
-    // dig(107, 16, 5000, 0.5);
-    // dig(7, 16, 2000, 1);
-    // dig(99, 16, 2000, 0.2);
-    // dig(10, 16, 2000, 1);
-    dig(77, 8, 1000, 0.5);
+    dig(100, 8, 2000, 0.1);
+    // dig(1, 16, 3000, 1);
     tic++;
   }
 }
 
-function dot(f) {
-  strokeWeight(2);
-  stroke(0);
-  point(width/2, f/10);
+function newKeys() {
+  keys = "Z5Z5QZ55".split('');
+  keys.reverse();
 }
 
-function dig(seed, ticSteps, freqRange, tStep) {
+function dig(seed, ticSteps, fRange, tStep) {
   noiseSeed(seed);
   if (tic < ticSteps) {
-    let freq = map(noise(t), 0, 1, -freqRange, freqRange);
+    let freq = map(noise(t), 0, 1, -fRange , fRange);
     osc.freq(freq, 0.2);
-    osc.amp(0.4, 0.2);
+    osc.amp(0.3, 0.2);
     t += tStep;
   } else {
     osc.stop();
     dug();
   }
-  print(freq);
-  return freq;
 }
 
 function dug() {
   let c = keys.pop();
-  if (c != null) {
+  if (c!=null) {
     t = unchar(c);
-    print(t);
     tic = 0;
     osc.start();
   } else {
@@ -62,18 +51,12 @@ function dug() {
 
 function keyTyped() {
   t = keyCode;
-  print(t);
   tic = 0;
   osc.start();
 }
 
-function newKeys() {
-  keys = "09D09DD".split('');
-  keys.reverse();
-}
-
 function mouseClicked() {
-  osc.start();
   isPlaying = true;
+  osc.start();
   newKeys();
 }
