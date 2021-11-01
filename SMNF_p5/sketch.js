f704a88a42056f251e6cd9f8b6abeb4adc883677
Seungbum Kim <picxenk@gmail.com>
2021-11-02 00:17:33 +0900

    add comments

--- SMNF_p5/sketch.js
+++ SMNF_p5/sketch.js
@@ -13,16 +13,22 @@ function draw() {
   background(freq/tic);
   text(char(t), width/2, height/2);
   if (isPlaying) {
-    dig(100, 16, 2000, 0.1);
+    dig(100, 16, 2000, 0.1); // 어떤 범위로 noise함수에서 소리를 채집할지 정합니다.
     tic++;
   }
 }
 
+// 각 키로 채집한 소리의 순서를 정합니다.
 function newKeys() {
   keys = "Z5Z5AZ55".split('');
   keys.reverse();
 }
 
+// noise함수에서 소리를 채집할 때 쓴 함수
+// seed : 노이즈 씨앗, 씨앗이 같으면 노이즈 함수는 같은 흐름의 값을 가집니다.
+// ticSteps : 몇 번 걸음으로 값을 채집할지
+// fRange : 소리 주파수 범위
+// tStep : 각 걸음의 채집 간격
 function dig(seed, ticSteps, fRange, tStep) {
   noiseSeed(seed);
   if (tic < ticSteps) {
@@ -36,6 +42,7 @@ function dig(seed, ticSteps, fRange, tStep) {
   }
 }
 
+// newKeys에서 설정한 키 순서를 반복시킵니다.
 function dug() {
   let c = keys.pop();
   if (c!=null) {
